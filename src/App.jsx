7977f20cff67d64a132dfeaab52ab089115f9d06
doron99 <maromdo@gmail.com>
2025-10-13 09:38:41 +0300

    changes for contextmenu

--- src/App.jsx
+++ src/App.jsx
@@ -14,16 +14,126 @@ import { AppFooter } from './cmps/AppFooter.jsx'
 import { WelcomePage } from './pages/WelcomePage.jsx'
 import { AppModal } from './cmps/AppModal.jsx'
 import { BoardIndex } from './pages/BoardIndex.jsx'
+import { ContextMenu } from './cmps/ContextMenu.jsx'
+import { useEffect, useRef, useState } from 'react'
 
 
 
 export default function App() {
+    const contextMenuRef = useRef(null)
+    const [contextMenu,setContextMenu] = useState(
+        {position: {
+            x:0,
+            y:0},
+            toggled:false})
+    const [people,setPeople] = useState([
+        {id:1,name:'johnny',selected:false},
+        {id:2,name:'johnny1',selected:false},
+        {id:3,name:'johnny2',selected:false},
+        {id:4,name:'johnny3',selected:false}])
+    function handleOnContextMenu(e, rightClickPerson) {
+        e.preventDefault();
+
+        const contextMenuAttr = contextMenuRef.current.getBoundingClientRect();
+        const isLeft = e.clientX < window.innerWidth / 2;
+
+        let x;
+        let y = e.clientY;
+
+        if (isLeft) {
+            x = e.clientX;
+        } else {
+            x = e.clientX - contextMenuAttr.width;
+        }
+        const t = {
+            position: {
+                x,
+                y,
+            },
+            toggled: true,
+        }
+        console.log(t);
+        setContextMenu(t);
+
+        console.log(rightClickPerson);
+    }
+    function _resetContextMenu() {
+        console.log('_resetContextMenu')
+        const t = {
+            position: {
+                x:0,y:0
+            },
+            toggled: false,
+        }
+        console.log(t);
+        setContextMenu(t);
+    }
+
+    useEffect(() => {
+        function handler(e) {
+            console.log(contextMenuRef.current)
+            console.log('e.target.dataset.id',e.target.dataset.id)
+            if (e.target.className == 'some1') {
+
+            } else {
+                _resetContextMenu();
+            }
+          
+        }
+
+        document.addEventListener('click', handler);
+        return () => {
+            document.removeEventListener('click', handler);
+        };
+    }, []);
+    const contextMenuItems =[
+                        {
+                            text: 'Do Something',
+                            icon: '👁️',
+                            onClick: () => {
+                                //alert('Action 1 executed');
+                                _resetContextMenu();
+                            },
+                            isSpacer: false
+                        },
+                        {
+                            text: 'Do Something Else',
+                            icon: '✏️',
+                            onClick: () => {
+                                //alert('Action 2 executed');
+                                _resetContextMenu();
+                            },
+                            isSpacer: false
+                        }
+                    ]
 
     return (
         <Provider store={store}>
             <Router>
+                {JSON.stringify(contextMenu,null,2)}
                 <section className="app">
                     <AppHeader />
+                    <ContextMenu 
+                    contextMenuRef={contextMenuRef}
+                    isToggled={contextMenu.toggled}
+                    positionX={contextMenu.position.x}
+                    positionY={contextMenu.position.y}
+                    
+                    >
+                         <ul>
+                    {contextMenuItems.map((item, index) => (
+                        <li key={index} onClick={item.onClick} style={{ cursor: 'pointer' }}>
+                            {item.text}
+                        </li>
+                    ))}
+                </ul>
+
+ </ContextMenu>
+
+                    {/* //onContextMenu for right click */}
+                    <div onClick={(e) => {handleOnContextMenu(e);}} 
+                    className='some1' data-id='asd'>
+                        something</div>
                     <main className='main-layout'>
                         <Routes>
                             <Route path="/" element={<WelcomePage />}  />
--- src/pages/BoardIndex.jsx
+++ src/pages/BoardIndex.jsx
@@ -4,12 +4,14 @@ import { useParams } from 'react-router-dom';
 import { BoardList } from '../cmps/BoardList.jsx'
 import { BoardDetails } from '../cmps/BoardDetails.jsx'
 import { AppSideBar } from '../cmps/AppSideBar.jsx'
+import { useState } from 'react';
 
 
 export function BoardIndex() {
     const isDev = useSelector(storeState => storeState.devToolModule.isDev)
     const {boardId} = useParams()
     console.log('boardId', boardId)
+
     const section = !boardId ? <BoardList/> : <BoardDetails />
     return (
         <main >
