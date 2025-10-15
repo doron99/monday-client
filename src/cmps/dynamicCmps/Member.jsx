703a4a4037975945fbe101b0ff9904455e4d20f4
doron99 <maromdo@gmail.com>
2025-10-15 19:18:36 +0300

    fix style in members popper

--- src/cmps/dynamicCmps/Member.jsx
+++ src/cmps/dynamicCmps/Member.jsx
@@ -1,7 +1,15 @@
 import { useEffect, useRef, useState } from "react";
 import { PopperPeople } from "../contextMenuCmps/PopperPeople";
 import { useSelector } from "react-redux";
+import { HiOutlineUserCircle } from "react-icons/hi2";
+import { FaPlus } from "react-icons/fa6";
 
+ const allPeople = [
+        { _id: 'u100',name: 'Doron test' },
+        { _id: 'u101',name: 'Gil test' },
+        { _id: 'u102',name: 'Mira test' },
+        // Add more people as needed
+    ];
 
 
 export function Member({ info,onTaskUpdate }) {
@@ -30,16 +38,17 @@ const devSection = isDev
   return (
     <div className="task-status">
     {devSection}
-      <button
+      {/* <button
         ref={buttonRef}
         onClick={() => handleOpen()} // Call handleOpen correctly
         className="status-button"
         style={{ backgroundColor: selected.color }}
-      >
-        {/* {selected.label} */}
-        {selected && selected.length > 0 ? selected.map(x => x).join(',') : '+'}
-      </button>
-      
+      > */}
+        {/* {selected && selected.length > 0 ? selected.map(x => x).join(',') : '+'} */}
+      {/* </button> */}
+      <div style={{cursor:'pointer'}} ref={buttonRef} onClick={() => handleOpen()}>{selected && selected.length > 0 ? allPeople.filter(person => selected.includes(person._id)).map(x =>  {
+        return <span title={x.name}><HiOutlineUserCircle style={{fontSize:'2rem'}}/></span>
+      }) : <FaPlus style={{fontSize:'2rem'}}  />}</div>
       <PopperPeople members={selected} strSelectedDate={selected}  isOpen={open} buttonRef={buttonRef} onSelect={(value) => onSelect(value)} onClose={() => handleClose()} />
     </div>
     );
