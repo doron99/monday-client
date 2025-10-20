f668b0c69e3b4a145398d4c534eea51208d3d60b
doron99 <maromdo@gmail.com>
2025-10-20 16:04:44 +0300

    fix member popper style in tasks and fix --var1 color

--- src/cmps/contextMenuCmps/PopperContentMember.jsx
+++ src/cmps/contextMenuCmps/PopperContentMember.jsx
@@ -52,40 +52,43 @@ export const PopperContentMember = ({ content, buttonRef, onSelect, onClose }) =
          <div>
             <div>
                 {/* <h3>Selected People</h3> */}
-                <div className="member-badge-list">
+                <div className="person-popper-badge-list">
                     {selectedPeople.map((person, index) => (
                       // style={{margin: '5px', padding: '5px', border: '1px solid #ccc'}}
-                        <span className="member-badge" key={index} >
-                            {person.name}&nbsp;<FaRegTimesCircle style={{cursor:'pointer'}} onClick={() => setSelectedPeople(selectedPeople.filter(p => p !== person))} />
+                        <span className="person-popper-badge" key={index} >
+                            {person.name}&nbsp;
+                            <span
+                             className="person-popper-remove" 
+                            style={{cursor:'pointer'}} 
+                            onClick={() => setSelectedPeople(selectedPeople.filter(p => p !== person))} 
+                            >✕</span>
                         </span>
                     ))}
                 </div>
             </div>
+            {selectedPeople.length > 0 
+              && <div style={styles.personPopperSearchSeperator}></div>}
             
-            {/* <input
-                type="text"
-                placeholder="Search names, roles or teams"
-                value={searchTerm}
-                onChange={handleSearch}
-            /> */}
-      <div class="search-container">
+      <div class="person-popper-search-container">
         <input
           type="text"
-          class="search-input"
+          class="person-popper-input"
           value={searchTerm}
           onChange={handleSearch}
           placeholder="Search names, roles or teams"
         />
-        <span class="search-icon">🔍</span>
+        {/* <span class="search-icon">🔍</span> */}
       </div>
-      <div className="suggested-container">
+      <div className="person-popper-suggested">
             {/* <h3>Suggested People</h3> */}
-              <div class="suggested-title">Suggested people</div>
+              <div class="person-popper-suggested-title">Suggested people</div>
 
             <div>
                 {filteredPeople.map((person, index) => 
                   !selectedPeople.some(selected => selected.name === person.name) && (
-                      <div onClick={() => handleAddPerson(person)} className="suggested-row" key={index}>
+                      <div 
+                      onClick={() => handleAddPerson(person)} 
+                      className="person-popper-suggested-row" key={index}>
                           <span>{person.name}</span>
                           {/* <button onClick={() => handleAddPerson(person)}>Add</button> */}
                       </div>
@@ -97,3 +100,12 @@ export const PopperContentMember = ({ content, buttonRef, onSelect, onClose }) =
     </div>
   );
 };
+const styles = {
+  personPopperSearchSeperator: {
+    height:'0.5rem',
+    width:'100%',
+    borderTop:'1px solid #d0d4e4',
+    marginTop:'0.5rem'
+
+  }
+}
