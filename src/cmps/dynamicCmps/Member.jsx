6eb17c20c98ef4d1e708106c1cc8361ef0bbc8f0
gilassen <gil.assenhaym@gmail.com>
2025-12-22 16:10:20 +0200

    Fix: show avatar for all selected members

--- src/cmps/dynamicCmps/Member.jsx
+++ src/cmps/dynamicCmps/Member.jsx
@@ -6,10 +6,11 @@ import { eventBusService } from "../../services/event-bus.service";
 import UserAvatarSvg from '../../assets/svgs/user-avatar.svg';
 
  const allPeople = [
-        { _id: 'u100',name: 'Doron test' },
-        { _id: 'u101',name: 'Gil test' },
-        { _id: 'u102',name: 'Mira test' },
-        // Add more people as needed
+        { _id: "u100", name: "Doron Cohen" },
+        { _id: "u101", name: "Gil Levi" },
+        { _id: "u102", name: "Mira Ben David" },
+        { _id: "u103", name: "Noam Shahar" },
+        { _id: "u104", name: "Yael Rosen" },
     ];
 
 
